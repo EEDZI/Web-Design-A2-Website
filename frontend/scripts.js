6bf1ce29c0312bd4d7bf5b7aa588af902aa66f96
Edz <edwardlovelock15@gmail.com>
2025-12-15 21:37:47 +0000

    Enhance index and services pages with improved layout and styling; add scroll animations

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,4 +1,23 @@
+// hamburger menu toggle
 function toggleSideNav() {
     const sideNav = document.getElementById("side-nav");
     sideNav.classList.toggle("translate-x-full");
 }
+
+//scroll fade animation
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.querySelectorAll(".scroll-fade-up");
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                }
+            });
+        },
+        {
+            threshold: 0.10 // trigger % value
+        }
+    );
+    element.forEach(el => observer.observe(el));
+});
